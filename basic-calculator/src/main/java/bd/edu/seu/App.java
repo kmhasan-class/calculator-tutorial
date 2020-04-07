effbf489235dd90a42226234730b2ea39acc3226
Monirul Hasan <kmhasan@seu.edu.bd>
2020-04-08 01:50:03 +0600

    designing the UI

--- basic-calculator/src/main/java/bd/edu/seu/App.java
+++ basic-calculator/src/main/java/bd/edu/seu/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"));
+        stage.setTitle("Basic Calculator");
         stage.setScene(scene);
         stage.show();
     }
