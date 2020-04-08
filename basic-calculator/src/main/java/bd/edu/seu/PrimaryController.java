85260d32acc8d44db6dda999aab840a4383f95e9
Monirul Hasan <kmhasan@seu.edu.bd>
2020-04-08 21:19:47 +0600

    adding functionality for the digit buttons

--- basic-calculator/src/main/java/bd/edu/seu/PrimaryController.java
+++ basic-calculator/src/main/java/bd/edu/seu/PrimaryController.java
@@ -1,9 +1,37 @@
 package bd.edu.seu;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
+    @FXML
+    private TextField displayField;
+
+    public PrimaryController() {
+        displayField = new TextField();
+    }
+
+    @FXML
+    private void handleDigitOperation(ActionEvent actionEvent) {
+        Button eventSource = (Button) actionEvent.getSource();
+        String digit = eventSource.getText();
+        String oldText = displayField.getText();
+        String newText = oldText + digit;
+        displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleZeroOperation() {
+        String oldText = displayField.getText();
+        if (!oldText.startsWith("0")) {
+            String newText = oldText + "0";
+            displayField.setText(newText);
+        }
+    }
 
     @FXML
     private void handleAdditionOperation() {
