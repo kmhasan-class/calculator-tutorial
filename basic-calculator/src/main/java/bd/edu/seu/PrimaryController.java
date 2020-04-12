cdc14cdb270a7d36c1cc9af4bf63f796d07a2b26
Monirul Hasan <kmhasan@seu.edu.bd>
2020-04-12 08:16:49 +0600

    adding functionality of operators

--- basic-calculator/src/main/java/bd/edu/seu/PrimaryController.java
+++ basic-calculator/src/main/java/bd/edu/seu/PrimaryController.java
@@ -11,6 +11,10 @@ public class PrimaryController {
     @FXML
     private TextField displayField;
 
+    private int number1;
+    private int number2;
+    private String operation;
+
     public PrimaryController() {
         displayField = new TextField();
     }
@@ -20,6 +24,8 @@ public class PrimaryController {
         Button eventSource = (Button) actionEvent.getSource();
         String digit = eventSource.getText();
         String oldText = displayField.getText();
+        if (oldText.startsWith("0"))
+            oldText = "";
         String newText = oldText + digit;
         displayField.setText(newText);
     }
@@ -35,6 +41,53 @@ public class PrimaryController {
 
     @FXML
     private void handleAdditionOperation() {
-        System.out.println("+");
+        // Step 1: Get the number typed into the displayField and copy it to somewhere
+        String numberAsString = displayField.getText();
+        number1 = Integer.parseInt(numberAsString);
+
+        // Step 2: Remember that it is an addition operation
+        operation = "ADDITION";
+
+        // Step 3: Clear the displayField so that we can read the next number
+        displayField.clear();
+    }
+
+    @FXML
+    private void handleSubtractionOperation() {
+        // Step 1: Get the number typed into the displayField and copy it to somewhere
+        String numberAsString = displayField.getText();
+        number1 = Integer.parseInt(numberAsString);
+
+        // Step 2: Remember that it is an addition operation
+        operation = "SUBTRACTION";
+
+        // Step 3: Clear the displayField so that we can read the next number
+        displayField.clear();
+    }
+
+    @FXML
+    private void handleEqualOperation() {
+        int result = 0;
+
+        // Step 1: Get the number typed into the displayField and copy it to somewhere
+        String numberAsString = displayField.getText();
+        number2 = Integer.parseInt(numberAsString);
+
+        // Step 2: Add the two numbers and store the result in a variable
+        switch (operation) {
+            case "ADDITION":
+                result = number1 + number2;
+                break;
+
+            case "SUBTRACTION":
+                result = number1 - number2;
+                break;
+
+            default:
+                break;
+        }
+
+        // Step 3: Display the result into the displayField
+        displayField.setText("" + result);
     }
 }
